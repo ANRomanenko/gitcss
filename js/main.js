a58bcfbf512a6629e01852547af1c143f8cb0139
romanenko <lifeapricot@gmail.com>
2021-05-04 16:41:25 +0300

    New files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(function(){
+    $('.top-slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow: '<button type="button" class="slick-arrow slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
 
+            }
+          },
+          {
+            breakpoint: 640,
+            settings: {
+                arrows:false
+            }
+          }
+        ]
+      });
 });
